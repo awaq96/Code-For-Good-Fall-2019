183f7080090b7eda70b76200ce79454800567eca
penguin-stack <josejimenez19855@gmail.com>
2019-10-18 21:43:41 -0500

    edited Company.java

--- team-17-EducationIsFreedom/team17/Company.java
+++ team-17-EducationIsFreedom/team17/Company.java
@@ -8,4 +8,53 @@ public class Company {
 	private int internAmt;
 	private InterviewSession[] interviewSessions = new InterviewSession[3];
 	
+	void setName(String name) {
+		
+	}
+	
+	String getName() {
+		return name;
+	}
+	
+	void setFirstDesiredStudents(String firstdesiredstudents) {
+		
+	}
+	
+	Student[] getFirstDesiredStudents() {
+		return firstDesiredStudents;
+	}
+	
+	void setSecondDesiredStudents(String seconddesiredstudents) {
+		
+	}
+	
+	Student[] getSecondDesiredStudents() {
+		return secondDesiredStudents;
+	}
+	
+	void setIndustry(String industry) {
+		
+	}
+	
+	String[] getIndustry() {
+		return industry;
+	}
+	
+	void setInternAmt(int internAmt) {
+		
+	}
+	
+	int getInternAmt() {
+		return internAmt;
+	}
+	
+	void setInterviewSessions(String interviewSessions) {
+		
+	}
+	
+	InterviewSession[] getInterviewSessions() {
+		return interviewSessions;
+	}
+	
+	
 }
