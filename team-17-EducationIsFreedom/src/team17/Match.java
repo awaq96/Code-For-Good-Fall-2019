b06670bd2b15d7ab744c520f1bdf77120be574e4
art160630 <art160630@utdallas.edu>
2019-10-18 23:39:53 -0500

    added null checking

--- team-17-EducationIsFreedom/src/team17/Match.java
+++ team-17-EducationIsFreedom/src/team17/Match.java
@@ -12,10 +12,14 @@ public class Match {
         for(int i = 0; i < companies.length; i++){
             for(int j = 0; j < companies[i].positions.length; j++){
                 for(int m = 0; m < students.length; m++){
-                    if(students[m].firstDesiredPositions[0].company == companies[i].getName() &&
-                            students[m].firstDesiredPositions[0] == companies[i].positions[j] &&
-                            companies[i].positions[j].secondDesiredStudents[0] == students[m]){
-                        report.put(companies[i].positions[j], students[m]);
+                    if(students[m] != null && companies[i].positions[j] != null){
+                        if(students[m].firstDesiredPositions[0].company == companies[i].getName() &&
+                                students[m].firstDesiredPositions[0] == companies[i].positions[j] &&
+                                companies[i].positions[j].secondDesiredStudents[0] == students[m]){
+                            report.put(companies[i].positions[j], students[m]);
+                            students[m] = null;
+                            companies[i].positions[j] = null;
+                        }
                     }
                 }
             }
