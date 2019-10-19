9867649195eccbae91bb77a0f9c8339e865c198a
art160630 <art160630@utdallas.edu>
2019-10-18 22:24:33 -0500

    changed IDE to intellij

---
+++ team-17-EducationIsFreedom/src/team17/Match.java
@@ -0,0 +1,14 @@
+package team17;
+import java.util.*;
+
+public class Match {
+    Map<Company, Student> report = new HashMap<Company, Student>();
+
+    void interviewScheduling(Student[] students, Company[] companies) {
+
+    }
+
+    void companyStudentMatching(Student[] students, Company[] companies) {
+
+    }
+}
